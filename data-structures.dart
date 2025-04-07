46b0e4301be91426c655fb7a119a9e2c8a348d5a
8lkc <papebilalysow@gmail.com>
2025-04-07 21:48:10 +0000

    🛒 ~ 𝟘𝟛

---
+++ data-structures.dart
@@ -0,0 +1,6 @@
+List<int> listNum = [1, 2, 3, 4, 5];
+List<Object> listObj = [1, 'two', 3.0, true];
+List<String> listStr = ['one', 'two', 'three'];
+List<List<Object>> listList = [listNum, listObj, listStr];
+Set<String> setStr = {'one', 'two', 'three'};
+Map<String, int> mapStr = {'one': 1, 'two': 2, 'three': 3};
