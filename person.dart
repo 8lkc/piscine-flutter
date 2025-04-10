e105647e64bc189fe5982e33f8f29e607de157d5
8lkc <papebilalysow@gmail.com>
2025-04-10 04:45:09 +0000

    🛒 ~ 𝟘𝟡

--- person.dart
+++ person.dart
@@ -1,13 +1,13 @@
 class Person {
-  String name;
-  String surname;
-  String cityOfOrigin;
-  int    age;
-  int    height;
+  final String name;
+  final String surname; // Optional (defaults to empty string)
+  final String cityOfOrigin;
+  final int age;
+  final int height;
 
   Person({
     required this.name,
-    required this.surname,
+    this.surname = '', // Optional parameter with default value
     required this.cityOfOrigin,
     required this.age,
     required this.height,
