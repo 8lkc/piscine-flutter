e45c6edfdf7f7525548af7fbb3fdcc26bf7bb6de
8lkc <papebilalysow@gmail.com>
2025-04-10 06:04:59 +0000

    🛒 ~ 𝟙𝟙

--- university.dart
+++ university.dart
@@ -1,17 +1,17 @@
 class University {
   // Attributes:
   String _name = "", _city = "";
-  int _ranking = 0;
+  int? _ranking = null;
 
   // Constructor:
   University(String name, String city, [int? ranking]) {
     _name = name;
     _city = city;
-    _ranking = ranking ?? 0;
+    _ranking = ranking ?? null;
   }
 
   // Getters:
   String get name => _name;
   String get city => _city;
-  int get ranking => _ranking;
+  int? get ranking => _ranking;
 }
