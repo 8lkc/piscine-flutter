6aa5197ae4f3f374e8ee515b98d9b56827a0b415
8lkc <papebilalysow@gmail.com>
2025-04-10 11:26:06 +0000

    🛒 ~ 𝟙𝟚

--- student.dart
+++ student.dart
@@ -1,4 +1,18 @@
-import 'person.dart';
+class Person {
+  String name = "";
+  String surname = "";
+  String cityOfOrigin = "";
+  int age = 0;
+  int height = 0;
+
+  Person(String name, String city, int age, int height, [String? surname]) {
+    this.name = name;
+    this.surname = surname ?? "";
+    this.cityOfOrigin = city;
+    this.age = age;
+    this.height = height;
+  }
+}
 
 class Student extends Person {
   // Attributes:
